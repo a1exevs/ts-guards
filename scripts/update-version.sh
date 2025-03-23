40df693fdf73a5725de9204520d975898b008058
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2025-03-23 12:07:21 +0300

    [Common] update-version.sh script (#47)

---
+++ scripts/update-version.sh
@@ -0,0 +1,23 @@
+# Merge branches
+git checkout develop
+git pull origin develop
+git checkout common/version-increase
+git pull origin common/version-increase
+git merge develop
+
+# Previous version in package.json
+prevVersion=$( sed -n -e 's/.*"version": "\(.*\)",/\1/p' package.json | head -1 )
+echo "Previous version: $prevVersion"
+
+# run version-increase script
+yarn version:patch
+
+# increased version in package.json
+newVersion=$( sed -n -e 's/.*"version": "\(.*\)",/\1/p' package.json | head -1 )
+echo "New version: $newVersion"
+
+git add package.json
+
+git commit -m "[Common] Version increase $newVersion"
+
+git push origin common/version-increase
